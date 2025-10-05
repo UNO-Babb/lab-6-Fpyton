f2a7f09778e4b0740e252b65067ad879c740b96e
Fpyton <fmolina@unomaha.edu>
2025-10-05 04:04:58 +0000

    Done DiceRoll%

--- DiceRoll.py
+++ DiceRoll.py
@@ -7,8 +7,9 @@ import random
 def main():
   #Create an empty list with possible roll values
   rolls = [0,0,0,0,0,0,0,0,0,0,0]
+  total_rolls = 10000
   #Create two dice values ranging from 1 - 6 each
-  for r in range(10000):
+  for r in range(total_rolls):
     dice1 = random.randint(1,6)
     dice2 = random.randint(1,6)
     total = dice1 + dice2
@@ -17,8 +18,10 @@ def main():
   
   #print statictics for dice rolls
   total_dice = 2
+  print("Total Count Percent")
   for count in rolls:
-    print(total_dice, ":", count)
+    percent = (count / total_rolls) * 100
+    print(total_dice, "   ", count, "   ", round(percent, 2), "%")
     total_dice = total_dice + 1
   
 if __name__ == '__main__':
